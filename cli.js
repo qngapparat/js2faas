2dc6c0bf0851bfbd085b7314a48d25e26adb83a4
qngapparat <jakob.wallnoefer@outlook.com>
2020-08-03 17:43:30 +0200

    pretty printing | remove --entry-file

--- cli.js
+++ cli.js
@@ -1,43 +1,43 @@
 #!/usr/bin/env node
+const arg = require('arg')
+const chalk = require('chalk')
+
 const amazon = require('./core/amazon')
 const google = require('./core/google')
 const ibm = require('./core/ibm')
 
 // TODO google, handle xml, multipart, etc https://cloud.google.com/functions/docs/writing/http
 
-const arg = require('arg')
+// Pretty printing
+const ERR = txt => chalk.bgRed.bold.black(' ERROR ') + ' ' + txt
+const HINT = txt => '💡 ' + txt
+console.error = (txt) => console.log(ERR(txt))
 
 const args = arg({
-  '--entry-file': String,
   '--name': String,
   '--runtime': String,
   '--aws-role': String
 })
 
-console.log("Tip: Make sure you're calling this in the directory of the function you want to port.")
-
-if (args['--entry-file'] == null) {
-  console.log('Specify --entry-file')
-  process.exit()
-}
+console.log(HINT("Make sure you're calling this in the directory of the function you want to port."))
 
 if (args['--name'] == null) {
-  console.log('Specify --name')
+  console.log(ERR('Specify --name'))
   process.exit()
 }
 
 if (/^[a-zA-Z0-9]+$/.test(args['--name']) === false) {
-  console.log('--name must be alphanumeric ([a-zA-Z0-9]+)')
+  console.log(ERR('--name must be alphanumeric ([a-zA-Z0-9]+)'))
   process.exit()
 }
 
 if (args['--aws-role'] == null) {
-  console.log('Specify --aws-role')
+  console.log(ERR('Specify --aws-role'))
   process.exit()
 }
 
 if (args['--runtime'] == null || /^nodejs8|nodejs10|latest$/.test(args['--runtime'] === false)) {
-  console.log('Specify --runtime (nodejs8 | nodejs10 | latest)')
+  console.log(ERR('Specify --runtime (nodejs8 | nodejs10 | latest)'))
   process.exit()
 }
 
