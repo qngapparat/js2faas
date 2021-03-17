dfbfb1f72f797676d3f1a954980810488d75bcc4
Jakob <jakob.wallnoefer@outlook.com>
2021-03-17 14:27:12 +0100

    Update hmm.js

--- core/hmm.js
+++ core/hmm.js
@@ -1,8 +1,8 @@
-exports.endpoint_test_1 = () => { }
+exports.endpoint_test_1 = () => { console.log("hi this is endpoint test 1") }
 
 
 module.exports = {
-  endpoint_test_2: () => { }
+  endpoint_test_2: () => { console.log("hi this endpoint test 2") }
   
 }
 
